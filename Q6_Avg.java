e30f3057633d7acaf8cb1c152109497506b4d9bc
akshaykumarbade <97378675+akshaykumarbade@users.noreply.github.com>
2022-01-10 09:24:01 +0530

    Add files via upload

---
+++ Q6_Avg.java
@@ -0,0 +1,16 @@
+class Q6_Avg
+{
+
+	public static void main(String args[])
+    {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+
+
+        double avg = (a + b +c)/3.0;
+		System.out.println(avg);
+    }
+
+}
